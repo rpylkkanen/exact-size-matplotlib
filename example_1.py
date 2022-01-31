36813ec9d8d03b29014864d8cbba8f60a94624ee
Robert Pylkkänen <robert.pylkkanen@gmail.com>
2022-01-31 18:11:34 +0200

    example 2

--- example_1.py
+++ example_1.py
@@ -1,33 +1,23 @@
 from alignment import Alignment
-import matplotlib
-import numpy
 
-# Example 1
-
+# Initialize the alignment (1x1 grid)
 nrows, ncols = 1, 1
 alignment = Alignment(nrows, ncols)
 
-fig = alignment.figure()
-axes = alignment.flatten()
-a = axes[0]
+# Configure spacing (in inches).
+a = alignment[0][0] # Custom Axes-object, we only have one.
+a.set_spacing(left=0.5, right=1.0, top=1.25, bottom=0.5)
 
-# Spacing
-left, right, top, bottom = 0.5, 1.0, 1.25, 0.5
-a.set_spacing(
-	left=left, 
-	right=right,
-	top=top, 
-	bottom=bottom
-)
-
-# Axis width
+# Configure axis size (in inches, calculated from final figure size).
 figure_width, figure_height = 3.0, 3.0
-width = figure_width - a.spacing_width()
-height = figure_height - a.spacing_height()
+width = figure_width - a.spacing_width() 	# = 1.5
+height = figure_height - a.spacing_height()	# = 1.25
 a.set_size(width=width, height=height)
 
-# Plotting
+# Initialize the matplotlib Axes-object and plot data.
 ax = a.matplotlib()
 ax.plot([1, 2, 3], [0.5, 0.25, 0.125])
 
+# Save the figure.
+fig = alignment.figure()
 fig.savefig('examples/example_1.png', dpi=100)
